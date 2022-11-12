e177fb70bad731d6a881e85dace582b71d9454da
Dayweed1 <106037061+Dayweed1@users.noreply.github.com>
2022-11-12 15:01:18 +0100

    Predkraj zavrseci

--- Gambla/Assets/MoveLoptica.cs
+++ Gambla/Assets/MoveLoptica.cs
@@ -1,37 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 
 public class MoveLoptica : MonoBehaviour
 {
-    public Rigidbody2D rigidbody;
-    public GameObject script;
-    float y = 0;
-    float brojac = 0;
-    float k;
-    [SerializeField]
-    float constantSpeed;
+    public Rigidbody2D rigidbody = new Rigidbody2D();
+    public GameObject text;
+    public float brojac = 0;
+    public float constantSpeed;
+    public float Koliko = 4;
+    public Sprite Sprite;
+
     void Update()
     {
         rigidbody.velocity = constantSpeed * (rigidbody.velocity.normalized);
-        rigidbody.angularDrag = 0;
-        rigidbody.angularVelocity = 0;
-      
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Smrt") GameObject.Destroy(rigidbody.gameObject);
+        //Udarac loptice u zemlju
+        if (collision.collider.tag == "Smrt") Destroy(rigidbody.gameObject);
+        //Udarac loptice u igraca
         if (collision.collider.tag != "Nepomerljiv") {
-            script.SetActive(true);
-            rigidbody.gravityScale = 0; 
+
+        if (GameObject.Find("Alo") == null) Spawn(ref Koliko,Sprite);
+
+           rigidbody.gravityScale = 0;
         }
-    if (collision.collider.tag == "Bam")
-            {
-            collision.collider.gameObject.SetActive(false); 
-                brojac++;
-                Debug.Log(brojac);
-            }
-        
+        //Udarac loptice u metu
+        if (collision.collider.tag == "Bam"){
+
+            Destroy(collision.collider.gameObject);
+            brojac++;
+            Debug.Log(brojac);
+       //     text.GetComponent<TextMeshPro>().text = brojac.ToString();
+     
+        }
+    
+    }
+   //Novi nivo
+    public void Spawn(ref float Koliko, Sprite Sprite)
+    {
+        System.Random r = new System.Random();
+        for (int i = 0; i < Koliko; i++)
+        {
+            GameObject a = new GameObject("Alo", typeof(BoxCollider2D), typeof(SpriteRenderer));
+            a.GetComponent<SpriteRenderer>().sprite = Sprite;
+            a.GetComponent<Transform>().localPosition = new Vector2((float)(r.NextDouble() * (10.107 + 10.107) - 10.107), (float)(r.NextDouble() * (4.5 - 0.5) + 0.5));
+            a.tag = "Bam";
+        }
+        Koliko++;
     }
 }
